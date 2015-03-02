2d508d8f7a33a1dc6b146bfa1ccd82ad32bd73b6
justinvdm <justinvdm.0.1.0@gmail.com>
2015-03-03 00:31:33 +0200

    implement the basics

--- sig.event.js
+++ sig.event.js
@@ -6,8 +6,184 @@
   else
     root.sig.event = factory(root.sig);
 }(this, function(sig) {
+  var setup = sig.setup,
+      teardown = sig.teardown,
+      put = sig.put,
+      protoSlice = Array.prototype.slice
+
+
+  var currentIdNum = 0
+  var types = {}
+  var typePriority = [
+    'dom',
+    'jquery',
+    'eventEmitter',
+    'd3Selection',
+    'd3Dispatcher'
+  ]
+
+
   function event() {
+    var s = sig()
+    var args = [listener].concat(slice(arguments))
+    listener.id = event.nextId()
+
+    var type = apply(inferType, args)
+    if (type === null) throw new Error("No listener type found")
+
+    setup(s, function() {
+      apply(type.on, args)
+    })
+
+    teardown(s, function() {
+      apply(type.off, args)
+    })
+
+    return s
+
+    function listener(e) {
+      put(s, e)
+    }
   }
 
+
+  function inferType() {
+    var result = null
+    var args = arguments
+
+    typePriority.some(function(typeName) {
+      var type = types[typeName]
+      if (!apply(type.is, args)) return false
+      result = type
+      return true
+    })
+
+    return result
+  }
+
+
+  types.dom = {}
+
+
+  types.dom.is = function(listener, obj, eventName) {
+    return hasMeth(obj, 'addEventListener')
+        && hasMeth(obj, 'removeEventListener')
+  }
+
+
+  types.dom.on = function(listener, obj, eventName) {
+    obj.addEventListener(eventName, listener)
+  }
+
+
+  types.dom.off = function(listener, obj, eventName) {
+    obj.removeEventListener(eventName, listener)
+  }
+
+
+  types.jquery = {}
+
+
+  types.jquery.is = function(listener, obj) {
+    return hasMeth(obj, 'on')
+        && hasMeth(obj, 'off')
+  }
+
+
+  types.jquery.on = function(listener, obj, eventName) {
+    obj.on(eventName, listener)
+  }
+
+
+  types.jquery.off = function(listener, obj, eventName) {
+    obj.off(eventName, listener)
+  }
+
+
+  types.eventEmitter = {}
+
+
+  types.eventEmitter.is = function(listener, obj) {
+    return hasMeth(obj, 'on')
+        && hasMeth(obj, 'removeListener')
+  }
+
+
+  types.eventEmitter.on = function(listener, obj, eventName, useCapture) {
+    obj.on(eventName, listener, useCapture)
+  }
+
+
+  types.eventEmitter.off = function(listener, obj, eventName) {
+    obj.removeListener(eventName, listener)
+  }
+
+
+  types.d3Dispatcher = {}
+
+
+  types.d3Dispatcher.is = function(listener, obj, eventName) {
+    return hasMeth(obj, 'on')
+        && hasMeth(obj, eventName)
+        && !hasMeth(obj, 'off')
+  }
+
+
+  types.d3Dispatcher.on = function(listener, obj, eventName) {
+    obj.on(d3EventKey(eventName, listener.id), listener)
+  }
+
+
+  types.d3Dispatcher.off = function(listener, obj, eventName) {
+    obj.on(d3EventKey(eventName, listener.id), null)
+  }
+
+
+  types.d3Selection = {}
+
+
+  types.d3Selection.is = function(listener, obj) {
+    return hasMeth(obj, 'selectAll')
+        && hasMeth(obj, 'data')
+        && hasMeth(obj, 'on')
+  }
+
+
+  types.d3Selection.on = function(listener, obj, eventName, useCapture) {
+    obj.on(d3EventKey(eventName, listener.id), listener, useCapture)
+  }
+
+
+  types.d3Selection.off = function(listener, obj, eventName) {
+    obj.on(d3EventKey(eventName, listener.id), null)
+  }
+
+
+  function d3EventKey(eventName, id) {
+    return [eventName, id].join('.')
+  }
+
+
+  function hasMeth(obj, name) {
+    return typeof (obj || 0)[name] == 'function'
+  }
+
+
+  function nextId() {
+    return 'sig-event' + currentIdNum++
+  }
+
+
+  function apply(fn, args) {
+    return fn.apply(this, args)
+  }
+
+
+  function slice(arr, start, end) {
+    return protoSlice.call(arr, start, end)
+  }
+
+
+  event.nextId = nextId
   return event
 }));
--- sig.event.test.js
+++ sig.event.test.js
@@ -1,2 +1,144 @@
+global.document = require('jsdom').jsdom()
+global.window = document.defaultView
+
+
+var assert = require('assert'),
+    $ = require('jquery'),
+    sig = require('sig-js'),
+    d3 = require('d3'),
+    EventEmitter = require('events').EventEmitter,
+    event = require('./sig.event'),
+    nextId = event.nextId,
+    then = sig.then,
+    reset = sig.reset
+
+
+function capture(s) {
+  var values = []
+
+  then(s, function(x) {
+    values.push(x)
+  })
+
+  return values
+}
+
+
+function createEvent(name) {
+  var e = document.createEvent('Event')
+  e.initEvent(name, true, true)
+  return e
+}
+
+
 describe("sig.event", function() {
+  var el
+
+  beforeEach(function() {
+    el = document.createElement('div')
+    document.body.appendChild(el)
+  })
+
+  afterEach(function() {
+    document.body.removeChild(el)
+    event.nextId = nextId
+  })
+
+  it("should support node.js event emitters", function() {
+    var emitter = new EventEmitter()
+    var s = event(emitter, 'foo')
+    var results = capture(s)
+
+    assert(!results.length)
+    emitter.emit('foo', {a: 23})
+    assert.deepEqual(results, [{a: 23}])
+
+    emitter.emit('foo', {b: 21})
+    assert.deepEqual(results, [{a: 23}, {b: 21}])
+
+    reset(s)
+    emitter.emit('foo', {b: 20})
+    assert.deepEqual(results, [{a: 23}, {b: 21}])
+  })
+
+  it("should support jquery selections", function() {
+    var $el = $(el)
+    var s = event($el, 'roar')
+    var results = capture(s)
+
+    var e1 = $.Event('roar')
+    assert(!results.length)
+    $el.trigger(e1)
+    assert.deepEqual(results, [e1])
+
+    var e2 = $.Event('roar')
+    $el.trigger(e2)
+    assert.deepEqual(results, [e1, e2])
+
+    var e3 = $.Event('roar')
+    reset(s)
+    $el.trigger(e3)
+    assert.deepEqual(results, [e1, e2])
+  })
+
+  it("should support d3 selections", function() {
+    event.nextId = function() { return 23 }
+
+    var selection = d3.select(el)
+    var s = event(selection, 'roar')
+    var results = capture(s)
+
+    assert(!results.length)
+    selection.on('roar.23')({a: 23})
+    assert.deepEqual(results, [{a: 23}])
+
+    selection.on('roar.23')({b: 21})
+    assert.deepEqual(results, [{a: 23}, {b: 21}])
+
+    reset(s)
+    assert(typeof selection.on('roar.23') == 'undefined')
+  })
+
+  it("should support d3 dispatchers", function() {
+    var dispatcher = d3.dispatch('roar')
+    var s = event(dispatcher, 'roar')
+    var results = capture(s)
+
+    assert(!results.length)
+    dispatcher.roar({a: 23})
+    assert.deepEqual(results, [{a: 23}])
+
+    dispatcher.roar({b: 21})
+    assert.deepEqual(results, [{a: 23}, {b: 21}])
+
+    reset(s)
+    assert.deepEqual(results, [{a: 23}, {b: 21}])
+  })
+
+  it("should support dom event listeners", function() {
+    var s = event(el, 'roar')
+    var results = capture(s)
+
+    var e1 = createEvent('roar')
+    assert(!results.length)
+    el.dispatchEvent(e1)
+    assert.deepEqual(results, [e1])
+
+    var e2 = createEvent('roar')
+    el.dispatchEvent(e2)
+    assert.deepEqual(results, [e1, e2])
+
+    var e3 = createEvent('roar')
+    reset(s)
+    el.dispatchEvent(e3)
+    assert.deepEqual(results, [e1, e2])
+  })
+
+  it("should throw an error if it can't find a listener type", function() {
+    assert.throws(test, /No listener type found/)
+
+    function test() {
+      event({}, 'foo')
+    }
+  })
 })
